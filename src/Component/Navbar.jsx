e40b4023d679a579f8b76cff5c0931bc1b1ae63e
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2025-04-20 01:12:47 +0530

    cleaned

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import search from '../assets/search.png';
-import logo from '../assets/Netflix-logo.png';
+import logo from '../assets/Netflix-Logo.png';
 
 import { useNavigate } from 'react-router-dom';
 const Navbar = ({color}) => {
